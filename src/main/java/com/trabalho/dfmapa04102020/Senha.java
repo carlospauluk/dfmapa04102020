af87b55ae42724b06510119be2ef5d97709f44a6
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2020-09-28 19:39:02 -0300

    ...

---
+++ src/main/java/com/trabalho/dfmapa04102020/Senha.java
@@ -0,0 +1,38 @@
+package com.trabalho.dfmapa04102020;
+
+/**
+ *
+ * @author Daniel F
+ */
+public class Senha {
+    
+    private int numero;
+    
+    private char tipo;
+
+    public Senha(int numero, char tipo) {
+        this.numero = numero;
+        this.tipo = tipo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public char getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(char tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+    
+    
+    
+}
